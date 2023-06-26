74fcd168e52e67ba4f01979f8cc18ddb4fc61e5d
Freddy-ux <alfredmuleri569@gmail.com>
2023-06-26 11:38:06 +0300

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+  resources :resturant_pizzas
+  resources :pizzas
+  resources :resturants
+  end
